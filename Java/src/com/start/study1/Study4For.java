74d1a8ab33a33fe0e300abd305ec657ec85cc7cc
dongil <az330015@gmail.com>
2023-12-05 14:27:53 +0900

    branch practice

--- Java/src/com/start/study1/Study4For.java
+++ Java/src/com/start/study1/Study4For.java
@@ -4,7 +4,9 @@ public class Study4For {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		for(int i = 0 ; i < 4 ; ++i) {
+			System.out.println(i);
+		}
 	}
 
 }
