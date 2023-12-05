9c0a3b3bba5608b303800cfc5912254920676f4d
dongil <az330015@gmail.com>
2023-12-05 11:52:42 +0900

    doing 'for' study

--- Java/src/com/start/study1/Study1For.java
+++ Java/src/com/start/study1/Study1For.java
@@ -1,5 +1,9 @@
 package com.start.study1;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class Study1For {
@@ -12,17 +16,25 @@ public class Study1For {
 		System.out.println("정수 하나를 입력하세요.");
 		long num = sc.nextLong();
 		
+		Queue<Long> lpq = new PriorityQueue<>();
+		
 		//입력받은 정수의 약수
 		//약수의 개수를 출력하세요
-		int count = 0;
 		for(long i = 1 ; i <= (long)Math.sqrt(num)/2 ; ++i) {
 			if(num%i == 0) {
-				System.out.println(i);
-				System.out.println(num/i);
-				count += (num/i == i)?1:2;
+				lpq.add(i);
+				
+				long other = num/i;
+				if(other != i) {
+					lpq.add(other);
+				}
 			}
 		}
-		System.out.println("약수의 개수 : " + count );
+		System.out.println("약수의 개수 : " + lpq.size() );
+		while(!lpq.isEmpty()) {
+			System.out.println(lpq.poll());
+		}
+		
 	}
 
 }
